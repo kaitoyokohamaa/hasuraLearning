23d3b0e2a3a9fd2e0ae46270ea3a94d7d767703a
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-10-16 14:14:17 +0900

    formatter

--- src/components/Todo/TodoPrivateList.js
+++ src/components/Todo/TodoPrivateList.js
@@ -2,25 +2,23 @@ import React, { useState, Fragment } from "react";
 
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
+import gql from "graphql-tag"
+ import { useQuery } from '@apollo/client';
 
+const GET_MY_TODOS = gql`
+query getMyTodos {
+  todos(where:{is_public:{_eq:false}},order_by:{created_at:desc}){
+    id
+    title
+    created_at
+    is_completed
+  }
+}
+`
 const TodoPrivateList = props => {
   const [state, setState] = useState({
     filter: "all",
     clearInProgress: false,
-    todos: [
-      {
-        id: "1",
-        title: "This is private todo 1",
-        is_completed: true,
-        is_public: false
-      },
-      {
-        id: "2",
-        title: "This is private todo 2",
-        is_completed: false,
-        is_public: false
-      }
-    ]
   });
 
   const filterResults = filter => {
@@ -31,12 +29,12 @@ const TodoPrivateList = props => {
   };
 
   const clearCompleted = () => {};
-
-  let filteredTodos = state.todos;
+  const {todos} = props; 
+  let filteredTodos = todos;
   if (state.filter === "active") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed !== true);
+    filteredTodos = todos.filter(todo => todo.is_completed !== true);
   } else if (state.filter === "completed") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed === true);
+    filteredTodos = todos.filter(todo => todo.is_completed === true);
   }
 
   const todoList = [];
@@ -60,5 +58,16 @@ const TodoPrivateList = props => {
     </Fragment>
   );
 };
+const TodoPrivateListQuery = () =>{
+    const [loading,error,data] = useQuery(GET_MY_TODOS);
 
-export default TodoPrivateList;
+    if(loading){
+      return <div>loading</div>
+    }
+    if(error){
+      return <div>Error!</div>
+    }
+    return <TodoPrivateList todos={data.todos} />
+}
+export default TodoPrivateListQuery;
+export  {GET_MY_TODOS}
